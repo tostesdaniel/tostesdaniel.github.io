a105a31e8df922d98e7d3923a04e259d65439c2b
Daniel Tostes <ohtosttnz@gmail.com>
2022-07-17 20:12:17 -0300

    feat: creates NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+export default function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
