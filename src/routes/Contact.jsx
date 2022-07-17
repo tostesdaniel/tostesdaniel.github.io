6c94b0e48effac3c94bde1d142e6cc79656463db
Daniel Tostes <ohtosttnz@gmail.com>
2022-07-17 17:11:27 -0300

    refactor: updates route components to use JSX transform
    https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#removing-unused-react-imports

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function Contact() {
   return (
     <div>Contact</div>
--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function Projects() {
   return (
     <div>Projects</div>
